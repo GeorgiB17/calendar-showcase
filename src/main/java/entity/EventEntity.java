2ceb6beb61b623e99c53c6cffcba2a6f3f9b2a4b
Georgi Bozhilski <st178041@stud.uni-stuttgart.de>
2025-07-23 17:21:31 +0200

    Added the User and Event entities

--- src/main/java/entity/EventEntity.java
+++ src/main/java/entity/EventEntity.java
@@ -1,5 +1,42 @@
 package entity;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "events")
 public class EventEntity {
-    
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long event_id;
+private String title;
+private String description;
+private LocalDateTime time;
+private String location;
+
+@OneToOne
+@JoinColumn(name = "user_id", nullable = false)
+private UserEntity creator;
+
+@ManyToMany
+@JoinTable(
+    name = "event_participants",
+    joinColumns = @JoinColumn(name = "event_id"),
+    inverseJoinColumns = @JoinColumn(name = "user_id")
+)
+private Set<UserEntity> participants;
+
+
+
 }
--- src/main/java/entity/UserEntity.java
+++ src/main/java/entity/UserEntity.java
@@ -1,5 +1,27 @@
 package entity;
+import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+    private String username;
+    private String email;
+    private String password;
+    private String fullName;
     
+
+    @ManyToMany(mappedBy = "participants")
+    private Set<EventEntity> eventsParticipated;
+
 }
+
