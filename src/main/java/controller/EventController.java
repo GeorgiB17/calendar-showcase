79b9308eb31b92e2e6d58cddcffb86b957c81c8a
Georgi Bozhilski <st178041@stud.uni-stuttgart.de>
2025-07-30 01:50:11 +0200

    Added Start for controllers

--- src/main/java/controller/EventController.java
+++ src/main/java/controller/EventController.java
@@ -1,5 +1,12 @@
 package controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+
+@RestController
+@RequestMapping("/api/events")
 public class EventController {
+
     
 }
