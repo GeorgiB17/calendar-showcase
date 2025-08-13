b8bb228a6bb807179121534c82cf129d603c763e
Georgi Bozhilski <st178041@stud.uni-stuttgart.de>
2025-08-13 17:34:48 +0200

    Step forward for calendar

--- src/main/java/frontend/react-calendar/src/components/Calendar.tsx
+++ src/main/java/frontend/react-calendar/src/components/Calendar.tsx
@@ -6,9 +6,12 @@ function Calendar() {
  const [currentDate, setCurrentDate] = useState(new Date());
  const monthNames = [
   "January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
+  "July", "August", "September", "October", "November", "December"];
 const daysInWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const hoursInDay = []
+for (let i = 6; i < 24; i++) {
+    hoursInDay.push(i < 10 ? `0${i}:00` : `${i}:00`);
+ }
 
  function forwardDate() {
     setCurrentDate(prevDate => { 
@@ -34,15 +37,39 @@ function backwardDate() {
   <table className="table table-bordered">   
      <thead>
      <tr>
-        <th id="emptySpace"></th>
-        {daysInWeek.map((day,index ) => { 
+        <th></th>
+        {
+         
+        daysInWeek.map((day,index ) => { 
+         const currentDayIndex = (new Date().getDay() + 6) % 7;
           const dateForDay = new Date(currentDate);
            dateForDay.setDate(dateForDay.getDate() + index);
 
-            return <th key={day}>{day} {dateForDay.getDate()} </th>
+            return <th style={{ textAlign: "center", backgroundColor: index === currentDayIndex ?
+               "lightblue" : "white"
+             }} key={day}>
+               {day} {dateForDay.getDate()} 
+            </th>
 
         })}
      </tr>
+     {hoursInDay.map((hour, hourIndex) => {
+        return <tr key={hourIndex}>
+            <td>{hour}</td>
+            {daysInWeek.map((day) => {
+            return <td key={day + hourIndex}></td>
+            }
+         )}
+
+        </tr>
+
+      })}
+     
+      
+      
+      
+     
+
      </thead>
 
      </table>
