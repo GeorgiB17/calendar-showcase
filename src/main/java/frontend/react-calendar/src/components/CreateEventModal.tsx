2be756d43583a0411ce2c62cd00344baa9faf6f6
Georgi Bozhilski <st178041@stud.uni-stuttgart.de>
2025-08-24 01:40:14 +0200

    Added Create event modal functionality

--- src/main/java/frontend/react-calendar/src/components/CreateEventModal.tsx
+++ src/main/java/frontend/react-calendar/src/components/CreateEventModal.tsx
@@ -10,11 +10,40 @@ export default function CreateEventModal({
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <h2>Create Event</h2>
-        <p>This is a simple modal.</p>
-        <button onClick={onClose} className="close-btn">
-          Close
-        </button>
+        <h2>Create an Event</h2>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+            marginBottom: "20px",
+          }}
+        >
+          <label htmlFor="event-title">Event Title:</label>
+          <input type="text" id="event-title" name="event-title" />
+          <label htmlFor="event-date">Event Date:</label>
+          <input type="date" id="event-date" name="event-date" />
+          <label htmlFor="event-time">Event Time:</label>
+          <input type="time" id="event-time" name="event-time" />
+          <label htmlFor="event-description">Event Description:</label>
+          <textarea id="event-description" name="event-description" rows={4} />
+        </div>
+
+        <div style={{ display: "flex", gap: "10px", justifyContent: "right" }}>
+          <button
+            onClick={onClose}
+            className="close-btn rounded-pill shadow-sm"
+          >
+            Close
+          </button>
+          <button
+            type="submit"
+            className="modal-btn rounded-pill px-4 shadow-sm"
+            id="modal-btn-submit"
+          >
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
