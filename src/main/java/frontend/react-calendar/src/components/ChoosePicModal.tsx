635c757d27a14e4963518afc537ac21188eeb58d
Georgi Bozhilski <st178041@stud.uni-stuttgart.de>
2025-08-30 03:45:26 +0200

    Setup for choosing/saving user picture

---
+++ src/main/java/frontend/react-calendar/src/components/ChoosePicModal.tsx
@@ -0,0 +1,23 @@
+type ChoosePicModal = {
+  onClose: () => void;
+};
+
+function ChoosePicModal({ onClose }: Readonly<ChoosePicModal>) {
+  return (
+    <>
+      <div className="modalOverlay">
+        <div className="modal-content">
+          <button
+            type="button"
+            onClick={onClose}
+            className="close-btn rounded-pill shadow-sm"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ChoosePicModal;
